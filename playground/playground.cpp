8cc1f1dee19438cb475ded610a086706f91065b6
Nathan Zhou <zzhou612@gmail.com>
2017-12-23 15:02:37 +0800

    feat: add topologicalSort function to playground

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -1,11 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <deque>
 #include "bnet.h"
+
+std::vector<BnetNodeID> topologicalSort(BooleanNetwork *net);
 
 int main() {
     BooleanNetwork *net;
-    std::string blif_file_path (PROJECT_SOURCE_DIR);
-    blif_file_path += "/playground/benchmark/alu4.blif";
+    std::string blif_file_path(PROJECT_SOURCE_DIR);
+    blif_file_path += "/playground/benchmark/C17.blif";
     net = new BooleanNetwork(blif_file_path);
-    net->PrintNetwork();
+
+    std::vector<BnetNodeID> sortedNodes;
+    sortedNodes = topologicalSort(net);
+
+    std::cout << "Topological Order of Nodes: ";
+    for (auto &i : sortedNodes) {
+        std::cout << i << ' ';
+    }
+
     delete (net);
     return 0;
+}
+
+std::vector<BnetNodeID> topologicalSort(BooleanNetwork *net) {
+    std::vector<BnetNodeID> sortedNodes;
+    std::deque<BnetNodeID> nodesQueue;
+    std::map<BnetNodeID, int> inDegrees;
+
+    for (BnetNode *t = net->getNodesList(); t != nullptr; t = t->next) {
+        inDegrees[t->name] = t->ninp;
+        if (t->ninp == 0)
+            nodesQueue.emplace_back(std::string(t->name));
+    }
+
+    while (!nodesQueue.empty()) {
+        BnetNodeID id = nodesQueue.front();
+        nodesQueue.pop_front();
+
+        BnetNode *t = net->getNodebyName(id);
+        sortedNodes.emplace_back(t->name);
+        for (int i = 0; i < t->nfo; i++) {
+            BnetNodeID outnode = t->outputs[i];
+            inDegrees[outnode] -= 1;
+            if (inDegrees[outnode] == 0)
+                nodesQueue.push_back(outnode);
+        }
+    }
+
+    return sortedNodes;
 }
