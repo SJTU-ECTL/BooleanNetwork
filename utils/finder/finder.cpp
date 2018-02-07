63722055923a1f6d54118f5a98ccfa4f3228b509
tripack45 <patrickyao@sjtu.edu.cn>
2018-02-07 11:05:20 +0800

    Incorportated 2 utilities: FileFinder & Stopwatch

---
+++ utils/finder/finder.cpp
@@ -0,0 +1,65 @@
+//
+// Created by tripack on 17-3-26.
+//
+
+#include <finder/finder.h>
+
+File::File(const std::string &base,
+           const std::string &ext)
+        : base(base), ext(ext) {}
+
+File File::operator[] (const std::string &name) const {
+    return File(base + "_" + name, ext);
+}
+
+File::operator std::string() const {
+    return this->toString();
+}
+
+std::string File::toString() const {
+    return base + ext;
+}
+
+File File::operator[](int i) const {
+    return this->operator[](std::to_string(i));
+}
+
+FileClass::FileClass(const std::string &ext)
+        : ext(ext) {
+    if (ext == "" || ext.front() == '.') return;
+    this->ext = "." + ext;
+}
+
+File FileClass::operator()(const std::string &name) const {
+    return File(name, ext);
+}
+
+std::string FileClass::toString() const {
+    return "FILENAME" + ext;
+}
+
+Path::Path(const std::string &p)
+        : path(p) {
+    while (!path.empty() && path.back() == '/' ) {
+        path.pop_back();
+    }
+    path += "/";
+}
+
+std::string Path::toString() const {
+    return path;
+}
+
+std::string Path::operator/(const std::string &f) const {
+    return path + f;
+}
+
+Path Path::sub(const std::string &f) const {
+    std::string s = f;
+    while (!s.empty() && s.front() == '/') {
+        s.erase(s.begin());
+    }
+    if (s.empty()) return *this;
+    return Path(this->path + s );
+}
+
---
+++ utils/finder/include/finder/finder.h
@@ -0,0 +1,58 @@
+//
+// Created by tripack on 17-3-26.
+//
+
+#ifndef VE490_RESOURCE_H
+#define VE490_RESOURCE_H
+
+#include <string>
+
+class File {
+    std::string base;
+    std::string ext;
+public:
+    File(const std::string& base, const std::string& ext);
+    File operator[] (const std::string &name) const;
+    File operator[] (int i) const;
+    operator std::string() const;
+    std::string toString() const;
+};
+
+class FileClass {
+    std::string ext;
+public:
+    FileClass(const std::string& ext);
+    File operator()(const std::string& name) const;
+    std::string toString() const;
+};
+
+class Path {
+    std::string path;
+public:
+    Path(const std::string& p);
+    std::string toString() const;
+    std::string operator/(const std::string& f) const;
+    Path sub(const std::string& f) const;
+};
+
+static const Path ProjectBase(PROJECT_SOURCE_DIR);
+static const Path DataPath = ProjectBase.sub("data");
+
+static const Path CircuitPath = DataPath.sub("circuit");
+
+static const Path BenchmarkPath     = CircuitPath.sub("other_original");
+static const Path BenchmarkAigPath  = CircuitPath.sub("other_fraig");
+
+static const Path McncPath          = CircuitPath.sub("mcnc_original");
+static const Path McncAigPath       = CircuitPath.sub("mcnc_aig");
+static const Path McncFraigPath     = CircuitPath.sub("mcnc_fraig");
+
+static const Path PrecompDbPath     = DataPath.sub("precomp");
+
+static const Path TempPath          = ProjectBase.sub("bin/temp");
+
+static const FileClass fBlif("blif");
+static const FileClass fCpp("cpp");
+static const FileClass fSo("so");
+
+#endif //VE490_RESOURCE_H
