c7afacb98e4ad2f3d6789479e41470d06eb79721
tripack45 <patrickyao@sjtu.edu.cn>
2018-02-08 15:35:08 +0800

    Introduced a Topsort Example to test BlifNetlist

--- examples/TopsortBlifNetlist/topsort.cpp
+++ examples/TopsortBlifNetlist/topsort.cpp
@@ -2,3 +2,31 @@
 // Created by Patrick Yao on 07/02/2018.
 //
 
+#include <finder/finder.h>
+#include <stopwatch/stopwatch.h>
+#include <ectlrf/blif_netlist.h>
+#include <iostream>
+
+using namespace std;
+using namespace ECTL::Utils;
+
+static Path CircuitsPath =
+        Path(RESOURCE_BASE_DIR)
+                .sub(CIRCUIT_BASE_DIR)
+                .sub("benchmark");
+
+int main() {
+    cout << CircuitsPath.toString() << endl;
+    cout << (CircuitsPath / fBlif("C880")) << endl;
+
+    ECTL::BlifNetlist netlist{};
+
+    netlist.loadFromBlif(CircuitsPath / fBlif("C880"));
+
+    for (auto gen = netlist.Inputs().generator(); !gen.hasEnded();)
+        cout << gen.next().name() << endl;
+
+    for (auto gen = netlist.Outputs().generator(); !gen.hasEnded();)
+        cout << gen.next().name() << endl;
+
+}
